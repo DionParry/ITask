9aa300028274e93d6da939222d26c078e4431bc5
Dion Parry <dionjohnparry@gmail.com>
2021-09-06 22:38:02 +0100

    Updated html to use mat table component
    * component updated to populate data into mat table.
    * html page updated to remove some of the demo information.

--- Angluar/my-app/src/app/app.component.ts
+++ Angluar/my-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { BooksReport } from 'src/booksReport';
 import { GetApiService } from './get-api.service'
 
 @Component({
@@ -6,14 +9,32 @@ import { GetApiService } from './get-api.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+// export class AppComponent {
+//   title = 'my-app';
+//   dataSource = [] as any;
+//   constructor(private api:GetApiService) {
+//     this.api.getData().subscribe(data => {
+//         console.warn(data);
+//         this.dataSource = data;
+//     })
+//   }
+
+
+export class AppComponent implements OnInit {
   title = 'my-app';
-  dataSource = [] as any;
-  constructor(private api:GetApiService) {
-    this.api.getData().subscribe(data => {
-        console.warn(data);
-        this.dataSource = data;
-    })
+  ELEMENT_DATA: BooksReport[] = [];
+  displayedColumns: string[] = ['id' ,'name', 'price', 'category','author'];
+  dataSource = new MatTableDataSource<BooksReport>(this.ELEMENT_DATA)
+  constructor(private api:GetApiService) {}
+
+  ngOnInit() {
+    this.getAllBooks();
   }
+
+  public getAllBooks() {
+    let books = this.api.getData();
+    books.subscribe(report=> this.dataSource.data=report as BooksReport[])
+  }
+
 
 }
